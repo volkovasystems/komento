f25b263f9f866196f65962af4ec16e43ac4ec470
johnlenon <johnlenonmaghanoy@gmail.com>
2017-01-29 13:03:20 +0800

    Add optimization.

---
+++ .install.sh
@@ -0,0 +1,25 @@
+yarn add \
+babel-runtime \
+handlebars \
+harden \
+protype \
+realign \
+stuffed \
+truly \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+handlebars \
+harden \
+protype \
+realign \
+stuffed \
+truly \
+--save
---
+++ komento-test.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const komento = require( "./komento.js" );
+
+let expression = function Merchant( ) { };
+result = komento( function procedure( ){ } );
