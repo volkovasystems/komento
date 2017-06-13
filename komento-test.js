09c31a57231d19cd03a7afd32ae8061e284517d2
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-13 20:24:14 +0800

    Updated server/client test.

--- komento-test.js
+++ komento-test.js
@@ -3,15 +3,16 @@ const komento = require( "./komento.js" );
 
 assert.equal( komento( function procedure( ){
 	return "hello world";
-} ), "hello world" );
+} ), "hello world", "should return 'hello world'" );
 
 assert.equal( komento( function procedure( ){
 	/*!
 		hello world yeah
 	*/
-} ), "hello world yeah" );
+} ), "hello world yeah", "should return 'hello world yeah'" );
 
-assert.equal( komento( "hello {{ world }}", { "world": "yeah" } ), "hello yeah" );
+assert.equal( komento( "hello {{ world }}", { "world": "yeah" } ), "hello yeah", "should return 'hello yeah'");
+
 assert.ok( komento( function( ){
 	return "hello world";
 } ) );
